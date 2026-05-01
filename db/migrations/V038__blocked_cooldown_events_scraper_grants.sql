09b450bfed77102e2c737bf948345cbb86880f78
Andrew Miller <miller.andrew.preston@gmail.com>
2026-05-01 08:48:19 -0500

    dealing with version conflict

--- db/migrations/V038__blocked_cooldown_events_scraper_grants.sql
+++
@@ -1,9 +0,0 @@
--- V038: Grant scraper_user INSERT + sequence access on staging.blocked_cooldown_events.
---
--- V027 granted SELECT + DELETE for the archiver flush path but omitted INSERT and
--- the sequence grant needed for the scraper's insert_blocked_cooldown_event writes.
--- This caused "permission denied for sequence blocked_cooldown_events_event_id_seq"
--- warnings logged as non-fatal in the scraper.
-
-GRANT INSERT ON staging.blocked_cooldown_events TO scraper_user;
-GRANT USAGE, SELECT ON SEQUENCE staging.blocked_cooldown_events_event_id_seq TO scraper_user;
